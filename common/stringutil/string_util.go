707ec599d5fad56ef3b5aca20fa07c8424587ed8
Cocoon <6631097@qq.com>
2021-07-16 10:21:22 +0800

    model name update

---
+++ common/stringutil/string_util.go
@@ -0,0 +1,44 @@
+package stringutil
+
+import (
+	"math/big"
+	"strconv"
+)
+
+// ReverseString 反转字符串
+func ReverseString(s string) string {
+	runes := []rune(s)
+	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
+		runes[from], runes[to] = runes[to], runes[from]
+	}
+	return string(runes)
+}
+
+// ToString 转换到字符串，兼容类型：int,float,error,bool
+func ToString(o interface{}) string {
+	switch o.(type) {
+	case int:
+		{
+			return strconv.Itoa(o.(int))
+		}
+	case float32, float64:
+		{
+			return big.NewFloat(o.(float64)).String()
+		}
+
+	case error:
+		{
+			return o.(error).Error()
+		}
+	case bool:
+		{
+			if o == true {
+				return "true"
+			} else {
+				return "false"
+			}
+
+		}
+	}
+	return ""
+}
--- go.mod
+++ go.mod
@@ -1,2 +1,5 @@
 module github.com/xuchongen/cocoon-go-util
+
 go 1.15
+
+require github.com/shopspring/decimal v1.2.0
